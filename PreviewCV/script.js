00f14ae1a75e1b6483b640a7d802e0e353ce3ea5
nguyendinhdat05 <dat09092005@gmail.com>
2025-03-23 23:43:04 +0700

    khá oce

--- PreviewCV/script.js
+++ PreviewCV/script.js
@@ -1,9 +1,9 @@
-const menuIcon = document.getElementById("menu-icon");
+// const menuIcon = document.getElementById("menu-icon");
 const overlay = document.getElementById("overlay");
 const closeBtn = document.getElementById("overlay-left");
 const overlaySelectedWork = document.getElementById("overlay-selected-work");
 const options = document.querySelectorAll(".option"); // Dùng querySelectorAll()
-const menuIconSelectedWork = document.getElementById("menu-icon-selected-work");
+// const menuIconSelectedWork = document.getElementById("menu-icon-selected-work");
 const navbarLeft = document.getElementById("navbar-left");
 
 const contactButton = document.getElementById("contact-click");
@@ -38,9 +38,9 @@ educationButton.addEventListener("click", () => {
     educationContent.scrollIntoView({ behavior: "smooth" });
 });
 
-menuIcon.addEventListener("click", () => {
-    overlay.classList.add("active");
-});
+// menuIcon.addEventListener("click", () => {
+//     overlay.classList.add("active");
+// });
 
 closeBtn.addEventListener("click", () => {
     overlay.classList.remove("active");
@@ -58,9 +58,9 @@ closeBtn.addEventListener("click", () => {
 });
 
 
-menuIconSelectedWork.addEventListener("click", () => {
-    overlay.classList.add("active");
-});
+// menuIconSelectedWork.addEventListener("click", () => {
+//     overlay.classList.add("active");
+// });
 
 navbarLeft.addEventListener("click", () => {
     overlaySelectedWork.classList.remove("active");
