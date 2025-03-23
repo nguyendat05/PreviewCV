a2aaa10710e4eb1394769af36a01b3d90b63cc0e
nguyendinhdat05 <dat09092005@gmail.com>
2025-03-23 22:34:17 +0700

    xong cuộn ở trang chính

--- PreviewCV/script.js
+++ PreviewCV/script.js
@@ -6,6 +6,37 @@ const options = document.querySelectorAll(".option"); // Dùng querySelectorAll(
 const menuIconSelectedWork = document.getElementById("menu-icon-selected-work");
 const navbarLeft = document.getElementById("navbar-left");
 
+const contactButton = document.getElementById("contact-click");
+const contactContent= document.getElementById("contact");
+
+const aboutMeButton = document.getElementById("about-me-click");
+const aboutMeContent= document.getElementById("about-me");
+
+
+const selectedWorkButton = document.getElementById("selected-work-click");
+const selectedWorContent= document.getElementById("selected-work");
+
+const educationButton = document.getElementById("education-click");
+const educationContent= document.getElementById("education");
+
+
+contactButton.addEventListener("click", () => {
+    contactContent.scrollIntoView({ behavior: "smooth" });
+});
+
+
+aboutMeButton.addEventListener("click", () => {
+    aboutMeContent.scrollIntoView({ behavior: "smooth" });
+});
+
+
+selectedWorkButton.addEventListener("click", () => {
+    selectedWorContent.scrollIntoView({ behavior: "smooth" });
+});
+
+educationButton.addEventListener("click", () => {
+    educationContent.scrollIntoView({ behavior: "smooth" });
+});
 
 menuIcon.addEventListener("click", () => {
     overlay.classList.add("active");
