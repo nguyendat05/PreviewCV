bcb17c8ec89212e9297fe041eb492279ed73f4f6
nguyendinhdat05 <dat09092005@gmail.com>
2025-03-23 18:43:00 +0700

    1

--- PreviewCV/script.js
+++ PreviewCV/script.js
@@ -1,6 +1,11 @@
 const menuIcon = document.getElementById("menu-icon");
 const overlay = document.getElementById("overlay");
 const closeBtn = document.getElementById("overlay-left");
+const overlaySelectedWork = document.getElementById("overlay-selected-work");
+const options = document.querySelectorAll(".option"); // Dùng querySelectorAll()
+const menuIconSelectedWork = document.getElementById("menu-icon-selected-work");
+const navbarLeft = document.getElementById("navbar-left");
+
 
 menuIcon.addEventListener("click", () => {
     overlay.classList.add("active");
@@ -9,3 +14,24 @@ menuIcon.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     overlay.classList.remove("active");
 });
+
+
+options.forEach((option) => {
+    option.addEventListener("click", () => {
+        overlaySelectedWork.classList.add("active");
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    overlaySelectedWork.classList.remove("active");
+});
+
+
+menuIconSelectedWork.addEventListener("click", () => {
+    overlay.classList.add("active");
+});
+
+navbarLeft.addEventListener("click", () => {
+    overlaySelectedWork.classList.remove("active");
+});
+
